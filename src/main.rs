677f8f594ad63ab4d5e13c3596ccdc8acd83e0a7
Christian Sakai <christianmsakai@gmail.com>
2019-01-23 16:57:29 -0500

    WIP

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,27 @@ use std::path::Path;
 use std::collections::VecDeque;
 
 fn main() {
+    let data = Data::new();
 
-    // let file_paths = depth_first_search_file("../provantagex");
-    // let total_loc = file_paths.iter()
-    //     .fold(0, |acc, file_path| {
-    //         if let Ok(count) = count_loc(file_path) {
-    //             return acc + count;
-    //         }
+    match data {
+        Err(err) => {
+            println!("{}", err);
+        },
+        Ok(data) => {
+            let file_paths = data.depth_first_search_file();
+            let total_loc = file_paths.iter()
+                .fold(0, |acc, file_path| {
+                    if let Ok(count) = count_loc(file_path) {
+                        return acc + count;
+                    }
 
-    //         acc
-    //     });
+                    acc
+                });
 
-    // println!("Total Lines of Code {:?}", total_loc);
+            println!("Total Lines of Code {:?}", total_loc);
+        }
+    }
+
 }
 
 struct Data {
@@ -27,67 +36,86 @@ struct Data {
 }
 
 impl Data {
-    fn new(args: Args) -> Result<Data, String> {
-        if let Some(root_path) = env::args()
+    fn new() -> Result<Data, String> {
+        let root_path = env::args()
             .skip(1)
             .find(|string| {
                 !string.starts_with("--b")
-            }) {
-            return Err(String::from("No root path provided"));
-        }
+            });
 
-        let mut ignores = Vec::new();
+        match root_path {
+            None => Err(String::from("No root path provided")),
+            Some(root_path) => {
+                let ignore_path = env::args()
+                    .skip(1)
+                    .find(|string| {
+                        string.starts_with("--b")
+                    });
 
-        // let ignore_path = env::args()
-        //     .skip(1)
-        //     .find(|string| {
-        //         string.starts_with("--b")
-        //     });
+                let mut ignores = Vec::new();
 
-        // if let Ok(mut file) = File::open(ignore_path) {
-        //     let mut contents = String::new();
-        //     if let Ok(_) = file.read_to_string(&mut contents) {
-        //         ignores = contents
-        //             .trim()
-        //             .split('\n')
-        //             .collect();
-        //     }
-        // }
+                if let Some(ignore_path) = ignore_path {
+                    if let Ok(mut file) = File::open(&ignore_path) {
+                        let mut contents = String::new();
+                        if let Ok(_) = file.read_to_string(&mut contents) {
+                            let contents = contents
+                                .trim()
+                                .split('\n');
 
-
-        Ok(Data {
-            root_path,
-            ignores,
-        })
-    }
-}
-
-fn depth_first_search_file(root_path: &str) -> Vec<String> {
-    let mut file_paths: Vec<String> = Vec::new();
-    let mut deque: VecDeque<String> = VecDeque::new();
-    deque.push_back(String::from(root_path));
-
-    while !deque.is_empty() {
-        let path_opt = deque.pop_front();
-        if let Some(path_str) = path_opt {
-            let path = Path::new(&path_str);
-            if path.is_dir() {
-                for entry in path.read_dir().expect("read_dir call failed") {
-                    if let Ok(entry) = entry {
-                        if let Some(path_str) = entry.path().to_str() {
-                            let path_str = String::from(path_str);
-                            deque.push_front(path_str);
+                            for content in contents {
+                                ignores.push(content.to_owned());
+                            }
                         }
                     }
+
                 }
 
-            } else if path.is_file() {
-                file_paths.push(path_str.clone());
+                Ok(Data {
+                    root_path,
+                    ignores,
+                })
             }
         }
     }
 
-    file_paths
+    fn depth_first_search_file(&self) -> Vec<String> {
+        let mut file_paths: Vec<String> = Vec::new();
+        let mut deque: VecDeque<String> = VecDeque::new();
+        deque.push_back(self.root_path.clone());
+
+        while !deque.is_empty() {
+            let path_opt = deque.pop_front();
+
+            // let x = self.ignores.iter().find(|string| {
+            //     string == path_opt
+            // });
+
+            // let root_path = env::args()
+            //     .skip(1)
+            //     .find(|string| {
+            //         !string.starts_with("--b")
+            //     });
+
+            if let Some(path_str) = path_opt {
+                let path = Path::new(&path_str);
+                if path.is_dir() {
+                    for entry in path.read_dir().expect("read_dir call failed") {
+                        if let Ok(entry) = entry {
+                            if let Some(path_str) = entry.path().to_str() {
+                                let path_str = String::from(path_str);
+                                deque.push_front(path_str);
+                            }
+                        }
+                    }
+
+                } else if path.is_file() {
+                    file_paths.push(path_str.clone());
+                }
+            }
+        }
+
+        file_paths
+    }
 }
 
 fn count_loc(file_path: &str) -> io::Result<usize> {
